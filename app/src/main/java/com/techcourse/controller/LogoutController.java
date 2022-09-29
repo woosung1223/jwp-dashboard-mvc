6def0d60c629cf2040f176ad79d17ce6db66c039
Donghyun Cho <devhudi@gmail.com>
2022-09-29 20:27:11 +0900

    [MVC 구현하기 - 3단계] 후디(조동현) 미션 제출합니다. (#297)
    * refactor: asis 디렉토리 삭제
    
    - 모든 컨트롤러를 애너테이션 기반으로 변경
    - ModelAndView에 jspView() 정적 팩토리 메소드 추가
    - 메인 페이지를 보여주는 MainController 추가
    - ManualHandlerMapping 제거
    
    * refactor: 폼 페이지 경로에서 /view 제거
    
    * refactor: 변수명 req, res 를 request, response로 통일
    
    * refactor: ObjectMapper를 JsonView의 상수로 관리

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -2,14 +2,18 @@ package com.techcourse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        final var session = req.getSession();
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final var session = request.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
+        return ModelAndView.jspView("redirect:/");
     }
 }
--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,30 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        String json = getJson(model);
+        writeJson(response, json);
+    }
+
+    private String getJson(final Map<String, ?> model) throws JsonProcessingException {
+        return OBJECT_MAPPER.writeValueAsString(model);
+    }
+
+    private void writeJson(final HttpServletResponse response, final String json) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.write(json);
     }
 }
