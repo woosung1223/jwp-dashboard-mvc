fabacbf5ef8169b3538070eb6aab2dc4bb789ce3
Maru <60432062+chawani@users.noreply.github.com>
2022-09-29 12:21:09 +0900

    [MVC 구현하기 - 3단계] 마루(송채원) 미션 제출합니다. (#282)
    * feat: json 형태로 응답을 반환하는 JsonView 클래스 구현
    
    * refactor: 레거시 코드 제거 및 클래스 패키지 이동
    
    * refactor: 불필요한 throws exception 제거
    
    * fix: 회원가입 시 유저 아이디가 증가하도록 수정

--- app/src/main/java/com/techcourse/controller/ForwardController.java
+++ app/src/main/java/com/techcourse/controller/ForwardController.java
@@ -7,10 +7,10 @@ import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
 
 @Controller
-public class RegisterViewController {
+public class ForwardController {
 
-    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
-    public String showRegisterPage(final HttpServletRequest req, final HttpServletResponse res) {
-        return "/register.jsp";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showRootPage(final HttpServletRequest request, final HttpServletResponse response) {
+        return "/index.jsp";
     }
 }
